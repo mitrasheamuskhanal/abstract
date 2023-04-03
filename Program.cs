200931090826f1a2d64c1832cf94dc3b701f0041
mitrasheamuskhanalgit config --global user.name mitrasheamuskhanalgit config --global user.name mitrasheamuskhanal <mitrasheamuskhanal@gmail.com>
2023-04-03 09:03:35 +0545

    just created

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿// C# program to calculate the area
-// of a square using the concept of
-// data abstraction
-using System;
+﻿using System;
 
 namespace Demoabstraction
 {
@@ -11,8 +8,8 @@ namespace Demoabstraction
     {
 
         // abstract method
-        public abstract int area(int l , int b);
-        public abstract int perimeter(int l , int b);
+        public abstract int area(int l, int b);
+        public abstract int perimeter(int l, int b);
     }
 
     // square class inheriting
@@ -26,44 +23,7 @@ namespace Demoabstraction
         }
         public override int perimeter(int l, int b)
         {
-            return 2*(l+b);
-        }
-
-        // private data member
-        private int side;
-
-        // method of square class
-        public Square(int x = 0)
-        {
-            side = x;
-        }
-
-        // overriding of the abstract method of Shape
-        // class using the override keyword
-        public override int area()
-        {
-            Console.Write("Area of Square: ");
-            return (side * side);
-        }
-    }
-
-    // Driver Class
-    class GFG
-    {
-
-        // Main Method
-        static void Main(string[] args)
-        {
-
-            // creating reference of Shape class
-            // which refer to Square class instance
-            Shape sh = new Square(4);
-
-            // calling the method
-            double result = sh.area();
-
-            Console.Write("{0}", result);
-
+            return 2 * (l + b);
         }
     }
 }
